8a96db7e097f5a97da9e0bfab31ab5293be5dbec
JSK <skruls.janis@gmail.com>
2023-06-07 15:00:54 +0300

    Add getComputerChoice function
    Function produces one of three possible values of rock, papers,
    scissors to be later compared to human choice.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 let playerSelection = prompt(`Select rock, paper or scissors`).toLowerCase();
+function getComputerChoice () {
+    let random = Math.floor(Math.random() * 100);
+    if(random >= 66) {
+        random = `scissors`;
+    }
+    else if(random >= 33) {
+        random = `paper`;
+    }
+    else {
+        random = `rock`;
+    }
+    return random;
+}
+getComputerChoice();
