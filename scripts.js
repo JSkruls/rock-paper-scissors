72f71f9bc6951b13be9cebf6be0cd89246488bb0
JSK <skruls.janis@gmail.com>
2023-06-07 16:08:04 +0300

    Add function that plays one round of the game
    Function compares computer and human choice to declare the winner.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let playerSelection = prompt(`Select rock, paper or scissors`).toLowerCase();
+
 function getComputerChoice () {
     let random = Math.floor(Math.random() * 100);
     if(random >= 66) {
@@ -12,4 +13,34 @@ function getComputerChoice () {
     }
     return random;
 }
-getComputerChoice();
+
+function playRound(humanChoice,computerChoice) {
+    if(humanChoice == computerChoice) {
+        return `It's a tie!`; 
+    }
+    else if(humanChoice == `rock`) {
+        if(computerChoice == `scissors`) {
+            return `You Win! Rock beats Scissors`;
+        }
+        else {
+            return `You Lose! Paper beats Rock`    
+        }
+    }
+    else if(humanChoice == `paper`) {
+        if(computerChoice == `rock`) {
+            return `You Win! Paper beats Rock`;
+        }
+        else {
+            return `You Lose! Scissors beat Paper`    
+        }
+    }
+    else if(humanChoice == `scissors`) {
+        if(computerChoice == `paper`) {
+            return `You Win! Scissors beat Paper`;
+        }
+        else {
+            return `You Lose! Rock beats scissors`    
+        }
+    }
+}
+playRound(playerSelection,getComputerChoice());
