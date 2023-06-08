f06c28a94094529ea98a87ab2a27788d8cfd4a8c
JSK <skruls.janis@gmail.com>
2023-06-08 12:46:35 +0300

    Make a function to play multiple rounds
    Function repeats a single round game to determine the overall winner
    of a series of games.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-let playerSelection = prompt(`Select rock, paper or scissors`).toLowerCase();
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
     let random = Math.floor(Math.random() * 100);
@@ -15,32 +16,57 @@ function getComputerChoice () {
 }
 
 function playRound(humanChoice,computerChoice) {
+    console.log(`h: ${humanChoice} c: ${computerChoice}`);
     if(humanChoice == computerChoice) {
+        humanScore++;
+        computerScore++;
         return `It's a tie!`; 
     }
     else if(humanChoice == `rock`) {
         if(computerChoice == `scissors`) {
+            humanScore++
             return `You Win! Rock beats Scissors`;
         }
         else {
-            return `You Lose! Paper beats Rock`    
+            computerScore++;
+            return `You Lose! Paper beats Rock`;    
         }
     }
     else if(humanChoice == `paper`) {
         if(computerChoice == `rock`) {
+            humanScore++
             return `You Win! Paper beats Rock`;
         }
         else {
-            return `You Lose! Scissors beat Paper`    
+            computerScore++;
+            return `You Lose! Scissors beat Paper`;    
         }
     }
     else if(humanChoice == `scissors`) {
         if(computerChoice == `paper`) {
+            humanScore++
             return `You Win! Scissors beat Paper`;
         }
         else {
-            return `You Lose! Rock beats scissors`    
+            computerScore++;
+            return `You Lose! Rock beats scissors`;    
         }
     }
 }
-playRound(playerSelection,getComputerChoice());
+
+function game(rounds) {
+    for(i = 0; i < rounds; i++) {
+        let playerSelection = prompt(`Select rock, paper or scissors`).toLowerCase();
+        playRound(playerSelection,getComputerChoice());
+    }
+    if(humanScore > computerScore) {
+        return `You've won with score of ${humanScore} : ${computerScore}`;   
+    }
+    else if(humanScore == computerScore){
+        return `It's a tie of ${humanScore} : ${computerScore}`; 
+    }
+    else {
+        return `You've lost with score of ${humanScore} : ${computerScore}`; 
+    }
+}
+game(5);
