607d44cc9c7f761b5567cd326771034ee9b620e2
JSkruls <62695130+JSkruls@users.noreply.github.com>
2023-10-01 08:24:19 +0300

    Add a html, css and js files to run the game
    Add a finished mpv version of "rock paper scissors".

--- scripts.js
+++ scripts.js
@@ -1,72 +1,132 @@
 let humanScore = 0;
 let computerScore = 0;
+let playerSelection;
 
 function getComputerChoice () {
-    let random = Math.floor(Math.random() * 100);
-    if(random >= 66) {
-        random = `scissors`;
-    }
-    else if(random >= 33) {
-        random = `paper`;
-    }
-    else {
-        random = `rock`;
-    }
-    return random;
+  let random = Math.floor(Math.random() * 100);
+  if(random >= 66) { 
+    random = `scissors`; 
+    computerHand.setAttribute('src','rps/scissors_red.png');
+  }
+  else if(random >= 33) { 
+    random = `paper`; 
+    computerHand.setAttribute('src','rps/paper_red.png');
+  }
+  else { 
+    random = `rock`; 
+    computerHand.setAttribute('src','rps/rock_red.png');
+  }
+  return random;
 }
 
 function playRound(humanChoice,computerChoice) {
-    console.log(`h: ${humanChoice} c: ${computerChoice}`);
-    if(humanChoice == computerChoice) {
-        humanScore++;
-        computerScore++;
-        return `It's a tie!`; 
-    }
-    else if(humanChoice == `rock`) {
-        if(computerChoice == `scissors`) {
-            humanScore++
-            return `You Win! Rock beats Scissors`;
-        }
-        else {
-            computerScore++;
-            return `You Lose! Paper beats Rock`;    
-        }
-    }
-    else if(humanChoice == `paper`) {
-        if(computerChoice == `rock`) {
-            humanScore++
-            return `You Win! Paper beats Rock`;
-        }
-        else {
-            computerScore++;
-            return `You Lose! Scissors beat Paper`;    
-        }
-    }
-    else if(humanChoice == `scissors`) {
-        if(computerChoice == `paper`) {
-            humanScore++
-            return `You Win! Scissors beat Paper`;
-        }
-        else {
-            computerScore++;
-            return `You Lose! Rock beats scissors`;    
-        }
-    }
+  if(humanChoice == computerChoice) {
+    return `It's a tie`; 
+  }
+
+  if(humanChoice == `rock` && computerChoice == `scissors`) {
+    humanScore++;
+    blueScore.textContent = humanScore;
+    return `Rock beats scissors`;
+  }
+  else if (humanChoice == `rock` && computerChoice == `paper`) {
+    computerScore++;
+    redScore.textContent = computerScore;
+    return `Paper beats rock`;    
+  }
+
+  if(humanChoice == `paper` && computerChoice == `rock`) {
+    humanScore++
+    blueScore.textContent = humanScore;
+    return `Paper beats rock`;
+  }
+  else if(humanChoice == `paper` && computerChoice == `scissors`) {
+    computerScore++;
+    redScore.textContent = computerScore;
+    return `Scissors beat paper`;    
+  }
+
+  if(humanChoice == `scissors` && computerChoice == `paper`) {
+    humanScore++
+    blueScore.textContent = humanScore;
+    return `Scissors beat paper`;
+  }
+  else if(humanChoice == `scissors` && computerChoice == `rock`) {
+    computerScore++;
+    redScore.textContent = computerScore;
+    return `Rock beats scissors`;    
+  }
 }
 
-function game(rounds) {
-    for(i = 0; i < rounds; i++) {
-        let playerSelection = prompt(`Select rock, paper or scissors`).toLowerCase();
-        playRound(playerSelection,getComputerChoice());
+function game() {
+  messageBox.textContent = playRound(playerSelection,getComputerChoice());
+  if(humanScore == 5 || computerScore == 5) {
+    lastLayer.classList.remove('hide');
+    if(humanScore == computerScore) {
+      return `It's a tie!`;   
     }
-    if(humanScore > computerScore) {
-        return `You've won with score of ${humanScore} : ${computerScore}`;   
-    }
-    else if(humanScore == computerScore){
-        return `It's a tie of ${humanScore} : ${computerScore}`; 
+    else if(humanScore > computerScore) {
+      return `You've won!`; 
     }
     else {
-        return `You've lost with score of ${humanScore} : ${computerScore}`; 
+      return `You've lost!`; 
     }
+  }
 }
-game(5);
+
+function makeInvisible() {
+  firstLayer.classList.add('hide');
+  midLayer.classList.toggle('hide');
+}
+
+function playerChoice() {
+  playerHand.classList.remove('hide');
+  computerHand.classList.remove('hide');
+  messageBox.classList.remove('hide');
+
+  if(this.getAttribute('class') == 'left button') {
+    playerSelection = 'rock';
+    playerHand.setAttribute('src','rps/rock_blue.png');
+  }
+  else if (this.getAttribute('class') == 'mid button') {
+    playerSelection = 'paper';
+    playerHand.setAttribute('src','rps/paper_blue.png');
+  }
+  else {
+    playerSelection = 'scissors';
+    playerHand.setAttribute('src','rps/scissors_blue.png');
+  }
+  gameResult.textContent = game();
+}
+
+function replayGame() {
+  humanScore = 0;
+  computerScore = 0;
+  blueScore.textContent = humanScore;
+  redScore.textContent = computerScore;
+  lastLayer.classList.add('hide');
+  playerHand.classList.add('hide');
+  computerHand.classList.add('hide');
+  messageBox.classList.add('hide');
+}
+
+const playButton = document.querySelector('.play');
+const firstLayer = document.querySelector('.first');
+const midLayer = document.querySelector('.mid');
+const lastLayer = document.querySelector('.last');
+const blueScore = document.querySelector('.score-blue');
+const redScore = document.querySelector('.score-red');
+const computerHand = document.querySelector('.computer-choice');
+const playerHand = document.querySelector('.player-choice');
+const messageBox = document.querySelector('.message');
+const gameResult = document.querySelector('.game-result');
+const playAgain = document.querySelector('.replay');
+const buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach(button => button.addEventListener('click',playerChoice));
+playButton.addEventListener('click',makeInvisible);
+playAgain.addEventListener('click',replayGame);
+
+// change alt text for images 
+// push project to github and upload to odin project
+// test on larger screen
